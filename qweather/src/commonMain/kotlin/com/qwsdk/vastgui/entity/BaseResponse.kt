6993b68f8d048a2625c8d200af0b3479265eab8d
vastgui <1550651926@qq.com>
2025-11-12 18:57:02 +0800

    update(project):基于文档 https://dev.qweather.com/docs/resource/error-code/ 部分 api 依旧会返回 code 字段，因此添加相关字段

--- qweather/src/commonMain/kotlin/com/qwsdk/vastgui/entity/BaseResponse.kt
+++ qweather/src/commonMain/kotlin/com/qwsdk/vastgui/entity/BaseResponse.kt
@@ -10,5 +10,19 @@ package com.qwsdk.vastgui.entity
  * @since 1.1.3
  */
 internal interface BaseResponse {
+    /**
+     * 目前同时存在两种版本的错误码，如果存在 [error]，
+     * 建议使用 [ErrorInfo.status] 。
+     *
+     * @since 1.1.3
+     */
+    @Deprecated(message = "建议使用 ErrorInfo.status", level = DeprecationLevel.WARNING)
+    val code: String? get() = null
+
+    /**
+     * [错误码](https://dev.qweather.com/docs/resource/error-code/) 。
+     *
+     * @since 1.1.3
+     */
     val error: ErrorInfo? get() = null
 }
